8321965e11c1dda36d13035a8515e63673bc34ae
Omololaomotalade <omololaomotalade@yahoo.com>
2024-01-22 15:00:07 +0100

    strDivMax

---
+++ strDivMax/strDivMax.js
@@ -0,0 +1,27 @@
+function processStrDivMax(str,div,maxDiv){
+  if (div > 0){
+    let strLength = Math.floor(str.length/div)
+    console.log(strLength);
+    let strStorage = [];
+
+    for(i = 0; i <str.length; i += strLength){
+      const arrChunkSize = strLength + i
+      console.log(arrChunkSize);
+      let chunk = str.substring(i, arrChunkSize)
+
+      if(chunk.length == strLength){
+        if(chunk.length > maxDiv){
+         chunk = chunk.substring(0, maxDiv)
+      }
+      strStorage.push(chunk)
+
+    }}
+    let backToString = strStorage.join('')
+
+    return backToString;
+  }
+}
+
+
+let strName = 'abcdefghijklmn';
+console.log(processStrDivMax(strName,3,2))
